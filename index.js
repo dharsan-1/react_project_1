2fd70e1121a082ccfa5f0f60f81c67c45055b343
dharsan-1 <dharsanramakrishnan@gmail.com>
2021-12-21 05:44:37 +0000

    new code added

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="background">
+    <h1 className="headin">super over league</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="img-con"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="img-con"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
